0cd3978003e39b54350c52397e55e03cfee134ca
Valla Hallama <valla.hallama@gmail.com>
2022-01-25 09:29:59 +0200

    invalid isoformat strings with numbers preceded by 0

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import re
 from datetime import datetime
 from fastapi import FastAPI
-from pydantic import BaseModel, ValidationError, validator
+from pydantic import BaseModel, validator
 from get_delivery_fee import get_delivery_fee
 
 class Order(BaseModel):
@@ -16,19 +16,22 @@ class Order(BaseModel):
 			raise ValueError('no items in order')
 		return v
 
-	# parsing input date string into a datetime object
+	# parsing input datetime string into a datetime object
 	# validation is strict by choice and accepts only full ISO format strings
-	# with timezone included and fractional seconds excluded
-	# eg. YYYY-MM-DDTHH:MM:SSZ or YYYY-MM-DDTHH:MM:SS±ZZ:ZZ
+	# with timezone included and optionally microseconds
+	# eg. YYYY-MM-DDTHH:MM:SS(.sss)Z or YYYY-MM-DDTHH:MM:SS(.sss)±ZZ:ZZ
+	# additional check is for incorrect dates such as Feb 30
 	@validator('time')
 	def validate_datetime(cls, v):
-		regex = r'^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(Z|[+-](2[0-3]|[01][0-9]):[0-5][0-9])$'
+		regex = r'^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](2[0-3]|[01][0-9]):[0-5][0-9])$'
 		match_iso8601 = re.compile(regex).match
 		if match_iso8601(v) is None:
 			raise ValueError('time not in ISO format, use: YYYY-MM-DDTHH:MM:SSZ or YYYY-MM-DDTHH:MM:SS±ZZ:ZZ')
-		return datetime.fromisoformat(v.replace('Z', '+00:00'))
-
-
+		try:
+			ret = datetime.fromisoformat(v.replace('Z', '+00:00'))
+		except:
+			raise ValueError('incorrect date')
+		return ret
 
 app = FastAPI()
 
--- get_delivery_fee.py
+++ get_delivery_fee.py
@@ -6,35 +6,44 @@ def get_delivery_fee(order):
 	with open("config.json", "r") as jsonfile:
 		rules = json.load(jsonfile, object_hook=lambda d: SimpleNamespace(**d))
 
-	#free_delivery
+	#free delivery
 	if order.cart_value >= rules.free_delivery_cutoff:
 		return 0
 
-	#small_order_surcharge
+	#small order surcharge
 	small_order_surcharge = rules.small_order_surcharge_cutoff - order.cart_value if order.cart_value < rules.small_order_surcharge_cutoff else 0
 
-	#delivery_distance_fee
+	#delivery distance fee
 	delivery_distance_fee = rules.delivery_distance.first_distance.value
 	dist = rules.delivery_distance.first_distance.meters
 	while dist < order.delivery_distance:
 		delivery_distance_fee += rules.delivery_distance.additional_distance.value
 		dist += rules.delivery_distance.additional_distance.meters
 
-	#item_surcharge
+	#item surcharge
 	item_surcharge = rules.item_surcharge.value * (order.number_of_items - rules.item_surcharge.cutoff) if order.number_of_items > rules.item_surcharge.cutoff else 0
 
+	#subtotal
 	fee = small_order_surcharge + delivery_distance_fee + item_surcharge
+
+	#friday rush
+	if order.time.weekday() == 4:
+		print(order.time.hour)
+		prefix = str(order.time.year) + '-' + str(order.time.month) + '-' + str(order.time.day) + 'T'
+		start = datetime.datetime.fromisoformat(prefix + rules.friday_rush.starting_time)
+		end = datetime.datetime.fromisoformat(prefix + rules.friday_rush.ending_time)
+		if order.time >= start and order.time < end:
+			fee = int(fee * rules.friday_rush.multiplier)
 
 	#maximum delivery fee
 	if fee > rules.maximum_delivery_fee:
 		fee = rules.maximum_delivery_fee
-
-	print(type(order.time))
-	print(order.time)
+	
 	print(
 f'small_order_surcharge_fee: {small_order_surcharge}\n\
 delivery_distance_fee: {delivery_distance_fee}\n\
-item_surcharge: {item_surcharge}')
+item_surcharge: {item_surcharge}\n\
+')
 	return fee
 
 if __name__=='__main__':
